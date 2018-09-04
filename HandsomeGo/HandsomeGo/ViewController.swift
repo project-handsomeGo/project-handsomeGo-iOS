f69bf79de4f84b8f417cd1ae2717a3a20e90ec25
pkse1213 <pkse1213@naver.com>
2018-09-04 21:28:50 +0900

    프로젝트 생성

---
+++ HandsomeGo/HandsomeGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HandsomeGo
+//
+//  Created by 박세은 on 2018. 9. 4..
+//  Copyright © 2018년 박세은. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
