bd871c6099076dfe56196914acbc59a4dc21a9c5
pkse1213 <pkse1213@naver.com>
2018-09-18 20:26:19 +0900

    지도 확대/스크롤

--- HandsomeGo/HandsomeGo/SEEUN/ViewControllers/ViewController.swift
+++ HandsomeGo/HandsomeGo/SEEUN/ViewControllers/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIScrollViewDelegate {
+    @IBOutlet weak var sc: UIScrollView!
+    @IBOutlet weak var imv: UIImageView!
+    
+    @IBOutlet weak var rightLayout: NSLayoutConstraint!
+//    @IBOutlet weak var leftLayout: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        rightLayout.constant = -229*self.view.frame.width/375
+        
+        let off = CGPoint(x: 75, y: 0)
+        sc.setContentOffset(off, animated: false)
+        print( sc.contentOffset.x )
+        sc.alwaysBounceVertical = false
+        sc.alwaysBounceHorizontal = false
+        sc.minimumZoomScale = 1.0
+        sc.maximumZoomScale = 3.0
+        sc.delegate = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let off = CGPoint(x: 75, y: 0)
+        sc.setContentOffset(off, animated: false)
     }
-
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(sc.contentOffset.x)
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.imv
+    }
 
 }
 
