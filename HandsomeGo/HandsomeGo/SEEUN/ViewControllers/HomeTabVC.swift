e3bd70723dcfca1a937458dd4e8192239217892f
pkse1213 <pkse1213@naver.com>
2018-09-23 16:51:26 +0900

    지도 뷰, 지도 디테일 뷰, 스탬프 완료

--- HandsomeGo/HandsomeGo/SEEUN/ViewControllers/HomeTabVC.swift
+++ HandsomeGo/HandsomeGo/SEEUN/ViewControllers/HomeTabVC.swift
@@ -9,24 +9,58 @@
 import UIKit
 
 class HomeTabVC: UIViewController {
-
+    var detailOpened = false {
+        didSet {
+            mapDetailView.isHidden = !detailOpened
+        }
+    }
+    
     @IBOutlet weak var mapBtn: UIButton!
     @IBOutlet weak var rankBtn: UIButton!
     @IBOutlet weak var mapUnderBar: UIView!
     @IBOutlet weak var rankUnderBar: UIView!
+   
+    // Map Outlet
+    @IBOutlet weak var mapScrollView: UIScrollView!
+    @IBOutlet weak var mapView: UIView!
+    @IBOutlet weak var mapImageView: UIImageView!
     
+    // Map Detail Outlet
+    @IBOutlet weak var mapDetailView: UIView!
+    @IBOutlet weak var detailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var detailBtn: UIButton!
+    
+    // Rank Outlet
     @IBOutlet weak var rankView: UIView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var rankTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapSetup()
+        mapDetailSetup()
+        rankSetup()
+    }
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        print("\(mapScrollView.contentOffset.x),\(mapScrollView.contentOffset.y)")
+        
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        mapScrollView.zoomScale = 1.5
+        mapScrollView.contentOffset = CGPoint(x: 95, y: 51)
+    }
+    
+    func rankSetup() {
         rankTableView.delegate = self
         rankTableView.dataSource = self
         
         imageView.layer.cornerRadius = 8 * self.view.frame.width/375
         imageView.layer.masksToBounds = true
-    
+        
         rankUnderBar.isHidden = true
         rankView.isHidden = true
     }
@@ -36,17 +70,64 @@ class HomeTabVC: UIViewController {
         rankUnderBar.isHidden = true
         rankView.isHidden = true
         
+        mapUnderBar.isHidden = false
+        mapScrollView.isHidden = false
+        
+        mapDetailView.isHidden = !detailOpened
+       
     }
     
     @IBAction func rankTapAction(_ sender: UIButton) {
         rankUnderBar.isHidden = false
         rankView.isHidden = false
         
-        
+        mapDetailView.isHidden = true
         mapUnderBar.isHidden = true
-        // mapView.isHidden = true
+        mapScrollView.isHidden = true
+    }
+    @IBAction func mapIconTapAction(_ sender: UIButton) {
+        detailOpened = true
     }
     
+    @objc func dismissView() {
+        detailOpened = false
+    }
+    
+    
+}
+
+
+extension HomeTabVC: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.mapView
+    }
+    
+    func mapSetup() {
+        
+        mapDetailView.isHidden = true
+        
+        let detailViewDismiss = UITapGestureRecognizer(target: self, action: #selector(dismissView))
+        self.mapScrollView.addGestureRecognizer(detailViewDismiss)
+        mapScrollView.delegate = self
+        mapScrollView.alwaysBounceVertical = false
+        mapScrollView.alwaysBounceHorizontal = false
+        mapScrollView.minimumZoomScale = 1.0
+        mapScrollView.maximumZoomScale = 3.0
+        mapScrollView.contentOffset = CGPoint(x: 74, y: 0)
+        
+    }
+    
+    func mapDetailSetup(){
+        mapDetailView.layer.shadowRadius = 6
+        mapDetailView.layer.shadowColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        mapDetailView.layer.shadowOffset = CGSize(width: 0, height: 3)
+        mapDetailView.layer.shadowOpacity = 0.15
+        
+        detailBtn.layer.cornerRadius = 27/2*self.view.frame.width/375
+
+    }
+        
+
 }
 
 extension HomeTabVC: UITableViewDelegate, UITableViewDataSource {
--- HandsomeGo/HandsomeGo/SEEUN/ViewControllers/QRcodeTabVC.swift
+++ HandsomeGo/HandsomeGo/SEEUN/ViewControllers/QRcodeTabVC.swift
@@ -9,106 +9,10 @@
 import UIKit
 
 class QRcodeTabVC: UIViewController {
-    @IBOutlet weak var rightLayout: NSLayoutConstraint!
-    
-    @IBOutlet weak var view2: UIView!
-    @IBOutlet weak var parentView: UIView!
-    
-    var lastScale: CGFloat!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let vc = UIStoryboard(name: "QRcode", bundle: nil).instantiateViewController(withIdentifier: "QRcodeVC")
-//        self.present(vc, animated: true, completion: nil)
-//
+        let vc = UIStoryboard(name: "QRcode", bundle: nil).instantiateViewController(withIdentifier: "QRcodeVC")
+        self.present(vc, animated: true, completion: nil)
     }
-    
-    @IBAction func drag(_ sender: UIPanGestureRecognizer) {
-   
-        let translation = sender.translation(in: self.view)
-       
-//        translation.x = max(translation.x, parentView.frame.minX - view2.frame.minX)
-//        translation.x = min(translation.x, parentView.frame.maxX - view2.frame.maxX)
-//
-//        translation.y = max(translation.y, parentView.frame.minY - view2.frame.minY)
-//        translation.y = min(translation.y, parentView.frame.maxY - view2.frame.maxY)
-
-        if let view = sender.view {
-            
-            var rec = view.frame;
-            let imgvw = parentView.frame;
-            if((rec.origin.x >= imgvw.origin.x && (rec.origin.x + rec.size.width <= imgvw.origin.x + imgvw.size.width)))
-            {
-                
-                let movement = translation;
-                view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y + translation.y)
-                
-                rec = view.frame;
-                
-                if rec.origin.x < imgvw.origin.x {
-                    rec.origin.x = imgvw.origin.x
-                    
-                }
-                
-                if rec.origin.x + rec.size.width > imgvw.origin.x + imgvw.size.width {
-                    rec.origin.x = imgvw.origin.x + imgvw.size.width - rec.size.width;
-                }
-                view.frame = rec;
-                
-//                view.center = CGPoint(x:view.center.x + translation.x, y:view.center.y + translation.y)
-                
-                sender.setTranslation(CGPoint.zero, in: self.view)
-                
-                
-            }
-        }
-        
-            
-    }
-
-    
-    @IBAction func zoomInOut(_ sender: UIPinchGestureRecognizer) {
-        if sender.state == .began {
-            lastScale = sender.scale
-        }
-        if let pinchView = sender.view , sender.state == .began || sender.state == .changed
-        {
-            let currentScale = (pinchView.layer.value(forKeyPath: "transform.scale") as AnyObject).floatValue
-            let kMaxScale:CGFloat = 3.0
-            let kMinScale:CGFloat = 1.0
-            
-            var newScale = 1.0 - (lastScale - sender.scale)
-            if let currentScale = currentScale {
-                newScale = min(newScale, kMaxScale / CGFloat(currentScale))
-                newScale = max(newScale, kMinScale / CGFloat(currentScale))
-                pinchView.transform = pinchView.transform.scaledBy(x: newScale, y: newScale)
-                sender.scale = 1.0
-                lastScale = sender.scale
-            }
-        }
-        
-        
-//
-//        if let view = sender.view {
-//
-//            if sender.state == .changed || sender.state == .ended {
-//                if sender.scale < 1 {
-//                    sender.scale = 1
-//                }
-//            }
-//
-//            view.transform = view.transform.scaledBy(x: sender.scale, y: sender.scale)
-//            sender.scale = 1
-//        }
-        
-       
-        
-    }
-        
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-       
-    }
-    
-
 }
